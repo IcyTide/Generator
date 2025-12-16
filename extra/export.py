2ae1d87dd35dc8658011d879ff6baa078729e08a
m0_37157335 <serfend@foxmail.com>
2025-12-16 22:24:31 +0800

    feat[equip]import by plugin

--- extra/export.py
+++ extra/export.py
@@ -1,4 +1,6 @@
+import copy
 import json
+import luadata
 
 from extra.attribute import Attribute
 from extra.gear import Gears
@@ -15,5 +17,31 @@ def main(info: dict):
         return None
 
 
-if __name__ == '__main__':
+def convert2jx3api(equips: list[dict]):
+    result = []
+    for e in equips:
+        if e.get("nPos") in {1}:
+            continue
+        data = copy.copy(e)
+        data["aSlotItem"] = [[-1, x] for x in e.get("aDiamondEnchant", [])]
+        color_stone = e.get("dwItemFEAEnchantID")
+        color_stone = {"0": [-1, color_stone]}
+        data["ColorInfo"] = color_stone
+        result.append(data)
+    return result
+
+
+def run_by_plugin_data(data: str):
+    import base64
+    import zlib
+
+    decoded_data = base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))
+    decompressed_data = zlib.decompress(decoded_data).decode("utf-8")
+    equips = luadata.unserialize(decompressed_data, encoding="utf-8", multival=False)
+    equips = convert2jx3api(equips)
+    attribute = main({"equip_data": equips})
+    print(attribute)
+
+
+if __name__ == "__main__":
     main(json.load(open("", encoding="utf-8")))
